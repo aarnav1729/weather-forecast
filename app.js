79e75b2ef24ead3cdb34868a31f5b01dd5a4a0fd
aarnav singh <aarnavsingh836@gmail.com>
2023-03-29 01:04:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,27 +3,36 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const weatherContainer = document.querySelector("#weather");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", (event) => 
+{
   event.preventDefault();
 
   const cityName = input.value.trim();
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`)
     .then((response) => response.json())
-    .then((data) => {
+    .then((data) => 
+    {
       console.log(data); // log the data received from the API for debugging purposes
 
       const cityName = data.name;
-      const temperature = data.main.temp;
+
+      const temperatureCelsius = data.main.temp;
+      const temperatureFahrenheit = ((temperatureCelsius * 9/5) + 32).toFixed(2);
+
       const description = data.weather[0].description;
 
       weatherContainer.innerHTML = `
+      <div id="weather-container">
         <h2>Weather in ${cityName}</h2>
-        <p>Temperature: ${temperature} &deg;C</p>
+        <p>Temperature: ${temperatureCelsius} &deg;C / ${temperatureFahrenheit} &deg;F</p>
         <p>Description: ${description}</p>
-      `;
+      </div>
+    `;    
     })
-    .catch((error) => {
+
+    .catch((error) => 
+    {
       console.error(error);
       weatherContainer.innerHTML = "<p>Unable to retrieve weather data.</p>";
     });
